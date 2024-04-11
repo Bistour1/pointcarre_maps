34484d1e77eba6a66b9e185646f0c945d1587d21
Bistouri <auguste.elengesa@gmail.com>
2024-04-11 13:26:52 +0200

    Premiers test sur les fichiers jpeg, corruption de fichier basique, on ne fait que changer quelques couleurs au hasard

--- main/map_to_disc.go
+++ main/map_to_disc.go
@@ -2,8 +2,19 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
+	"os"
+	"reflect"
 )
 
 func main() {
-	fmt.Println("hi")
+	filepath := "ressources/plan-brussels.jpg"
+	data, err := os.ReadFile(filepath)
+	fmt.Println(len(data))
+	fmt.Println(err)
+	fmt.Println(reflect.TypeOf(data))
+	for i := 5000; i < len(data); i += 50000 {
+		data[i] = byte(rand.Intn(200))
+	}
+	os.WriteFile("plan2.jpg", data, os.ModePerm)
 }
